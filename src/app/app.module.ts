ea6260cb3d0512168e4c5821812d05f1e04b4f29
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-08-29 15:21:17 +0700

    getting place details from firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChooseTagComponentComponent } from './choose-tag-component/choose-tag-component.component';
 import { ResultsPageComponent } from './results-page/results-page.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCI6EVaqRT6Df551Bfp4CQdPDRSqrzWnnU",
+  authDomain: "ga-web-test.firebaseapp.com",
+  databaseURL: "https://ga-web-test.firebaseio.com",
+  projectId: "ga-web-test",
+  storageBucket: "ga-web-test.appspot.com",
+  messagingSenderId: "468811037033",
+  appId: "1:468811037033:web:f07b9e20096c4f15d665f8",
+  measurementId: "G-Y26S1H4CJS"
+};
 
 @NgModule({
   declarations: [
@@ -24,7 +38,9 @@ import { ResultsPageComponent } from './results-page/results-page.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/results-page/results-page.component.ts
+++ src/app/results-page/results-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
+import { take, first } from 'rxjs/operators';
+import { isNull, isUndefined } from 'util';
 
 @Component({
     selector: 'app-results-page',
@@ -9,16 +13,31 @@ export class ResultsPageComponent implements OnInit {
 
     data = {}
     re = RegExp(' ',"g")
+    details = {}
     
-    constructor() { }
+    constructor(private db: AngularFirestore) { }
 
     ngOnInit(): void {
         this.data = JSON.parse(localStorage.getItem('results'))
-        console.log(this.data)
+        // console.log(this.data)
+        this.fireQuery(this.getDays())
+        console.log(this.details)
     }
 
     getDays(){
         return this.data["0"]['per-day-route']
+    }
+
+
+    fireQuery(dRoute){
+        for(let x of dRoute){
+            for(let y of x ){
+                console.log(y)
+                this.db.collection('POI', ref => ref.where('name', '>=', y).limit(1)).valueChanges().subscribe(data => {
+                    this.details[y] = data[0]
+                })
+            }
+        }
     }
 
 }
